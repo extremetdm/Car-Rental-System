d22d98ad646b6423770bdee3f89e3e086f5ab5ba
JosephIsNoob <unidentifiedperson117@gmail.com>
2024-04-16 17:13:34 +0800

    further work segregation

--- main.py
+++ main.py
@@ -17,17 +17,130 @@ def login():
   
   return loginStatus
 
-def managerMenu(user:Staff):
+def updateProfile(user:Staff):
   pass
+
+def managerMenu(user:Staff):
+  while True:
+    print('1.\tUpdate own profile')
+    print('2.\tRegister new staff')
+    print('3.\tUpdate existing staff record')
+    print('4.\tDelete staff record')
+    print('5.\tUpdate car renting rate') # who has the say? why default 250 n then manager can set other default? then car service staff can change???
+    print('6.\tView monthly revenue report')
+    print('7.\tExit program')
+    while (operation := input('\nEnter operation number: ')) not in ('1','2','3','4','5','6','7'):
+      print('\nInvalid operation number!')
+    match operation:
+      case '1':
+        updateProfile(user)
+
+      case '2':
+        pass
+
+      case '3':
+        pass
+
+      case '4':
+        pass
+
+      case '5':
+        pass
+
+      case '6':
+        pass
+
+      case '7':
+        return
+      
 
 def customer1Menu(user:Staff):
-  pass
+  while True:
+    print('1.\tUpdate own profile')
+    print('2.\tRegister new customer')
+    print('3.\tView registered customers')
+    print('4.\tUpdate existing customer record')
+    print('5.\tDelete customer record')
+    print('6.\tExit program')
+    while (operation := input('\nEnter operation number: ')) not in ('1','2','3','4','5','6'):
+      print('\nInvalid operation number!')
+    match operation:
+      case '1':
+        updateProfile(user)
+
+      case '2':
+        pass
+
+      case '3':
+        pass
+
+      case '4':
+        pass
+
+      case '5':
+        pass
+
+      case '6':
+        return
 
 def customer2Menu(user:Staff):
-  pass
+  # may subject to change because it is kinda unclear what is expected from the program here
+  while True:
+    print('1.\tUpdate own profile')
+    print('2.\tRecord new rental request')
+    print('3.\tGenerate bill')
+    print('4.\tView rental transactions')
+    print('5.\tDelete rental record')
+    print('6.\tExit program')
+    while (operation := input('\nEnter operation number: ')) not in ('1','2','3','4','5','6'):
+      print('\nInvalid operation number!')
+    match operation:
+      case '1':
+        updateProfile(user)
+
+      case '2':
+        pass
+
+      case '3':
+        pass
+
+      case '4':
+        pass
+
+      case '5':
+        pass
+
+      case '6':
+        return
 
 def carMenu(user:Staff):
-  pass
+  while True:
+    print('1.\tUpdate own profile')
+    print('2.\tRegister new car')
+    print('3.\tView car record')
+    print('4.\tUpdate existing car record')
+    print('5.\tDelete car record')
+    print('6.\tExit program')
+    while (operation := input('\nEnter operation number: ')) not in ('1','2','3','4','5','6'):
+      print('\nInvalid operation number!')
+    match operation:
+      case '1':
+        updateProfile(user)
+
+      case '2':
+        pass
+
+      case '3':
+        pass
+
+      case '4':
+        pass
+
+      case '5':
+        pass
+
+      case '6':
+        return
 
 if __name__ == '__main__':
   
